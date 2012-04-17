eaad64af6f0883edff209dae640917c37929f379
SvartalF <self@svartalf.info>
2012-04-17 14:03:12 +0900

    Overwritable storage for files

---
+++ utils/files/storage/overwrite.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+
+    def _save(self, name, content):
+        if self.exists(name):
+            self.delete(name)
+        return super(OverwriteStorage, self)._save(name, content)
+
+    def get_available_name(self, name):
+        return name
