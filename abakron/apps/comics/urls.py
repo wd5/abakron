33a28f9a52487e9e1bb8c9ff55de7917166bd165
SvartalF <self@svartalf.info>
2012-04-21 14:42:51 +0900

    Link to the latest comics in the navigation bar

--- abakron/apps/comics/urls.py
+++ abakron/apps/comics/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('comics.views',
-    (r'^$', 'index'),
+    url(r'^$', 'index', name='comics.index'),
     url(r'^(?P<chapter_slug>[\w-]+)/$', 'chapter', name='comics.chapters.read'),
     url(r'^(?P<chapter_slug>[\w-]+)/(?P<comics_position>\d+)/$', 'read', name='comics.read'),
 )
