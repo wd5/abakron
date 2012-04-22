8a5b18af2a9071d090b7b78938bb9b42fcc78386
SvartalF <self@svartalf.info>
2012-04-22 14:29:17 +0900

    Markdown representation of the FAQ's question in the admin

--- abakron/apps/faq/admin.py
+++ abakron/apps/faq/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+import markdown
 
 from faq.models import Entry
 from faq.forms import EntryAdminForm
@@ -9,12 +10,17 @@ from faq.forms import EntryAdminForm
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('user', 'have_answer', 'created')
     list_select_related = True
-    fields = ('question', 'answer')
-    readonly_fields = ('question',)
+    fields = ('question_markdown', 'answer')
+    readonly_fields = ('question_markdown',)
     form = EntryAdminForm
 
     def have_answer(self, obj):
         return len(obj.answer) > 0
     have_answer.boolean = True
+
+    def question_markdown(self, obj):
+        return markdown.markdown(obj.question)
+    question_markdown.short_description = u'Question'
+    question_markdown.allow_tags = True
 
 admin.site.register(Entry, EntryAdmin)
